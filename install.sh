4ff4020e6ddf0e5220cca98ebc62133d3b5209cc
shootingcharlie8 <melidosian.charlie@gmail.com>
2014-02-21 20:02:30 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+// nstall apache2
+sudo apt-get install apache2
+// install php
+sudo apt-get install php5
+// linking php and apache2
+sudo apt-get install libapache2-mod-php5
+// restarting apache2
+sudo /etc/init.d/apache2 restart
